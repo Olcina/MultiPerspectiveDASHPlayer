99631a01416a13285dc295a4c89df7bad37afc54
Carlos Olcina <karlos.olcina@gmail.com>
2018-01-15 11:08:45 +0100

    refactor: keyBindings.js file created

--- src/js/main.js
+++ src/js/main.js
@@ -81,56 +81,7 @@ function getInitialTime() {
 }
 
 
-window.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-        return; // Do nothing if the event was already processed
-    }
 
-    switch (event.key) {
-        case '0':
-            controler.add(0, url)
-            refreshLayout(layout, controler)
-            break;
-        case '1':
-            controler.add(1, url)
-            refreshLayout(layout, controler)
-            break;
-        case '2':
-            controler.add(2, url)
-            refreshLayout(layout, controler)
-            break;
-        case '3':
-            controler.add(3, url)
-            refreshLayout(layout, controler)
-            break;
-        case 's':
-            console.log(controler.getPlayerStatus());
-            
-        case 'm':
-            controler.toogleGlobalMute();
-            break;
-        case "ArrowLeft":
-            // Do something for "left arrow" key press.
-
-            controler.seekBackward(10);
-            break;
-        case "ArrowRight":
-            // Do something for "right arrow" key press.
-            controler.seekForward(10)
-            break;
-        case " ":
-            // Do something for "espace" key press.
-            controler.tooglePlay();
-            layout.playButton(controler.controllerPlaying);
-            break;
-        default:
-            console.log(event.key)
-            return; // Quit when this doesn't handle the key event.
-    }
-
-    // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-}, true);
 
 
 
