2994067453a9dfba80931416b29741b4ef2dc159
Carlos Olcina <karlos.olcina@gmail.com>
2018-01-18 19:08:13 +0100

    chore: browserify task on all.js
    removed gruntfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,10 @@ const del = require('del');
 const developServer = require('gulp-develop-server');
 const runSequence = require('run-sequence');
 
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+
 
 // Clean the folder build to start fresh
 gulp.task('clean', function (done) {
@@ -69,7 +73,7 @@ gulp.task('copy-sw', function () {
 
 // Precompile the templates into .js files to be inyected in the browser
 gulp.task('templates', function () {
-    return gulp.src('src/templates/*.hbs')
+    return gulp.src('src/templates/**/*.hbs')
         .pipe(handlebars())
         .on('error', util.log.bind(util))
         .pipe(through.obj(function (file, enc, callback) {
@@ -102,17 +106,34 @@ gulp.task('server', function () {
 
 // Watchers for the src folder
 gulp.task('watch', function () {
-    gulp.watch(['src/scss/**/*.scss'], ['compile-sass']);
-    gulp.watch(['src/templates/*.hbs'], ['templates']);
+    gulp.watch(['src/scss/*.scss'], ['compile-sass']);
+    gulp.watch(['src/templates/**/*.hbs'], ['templates']);
     gulp.watch(['src/sw/*.js'], ['copy-sw']);
-    gulp.watch(['src/js/*.js'], ['scripts-dist']);
+    gulp.watch(['src/js/*.js'], function () { runSequence('scripts-dist', 'browserifing') });
     gulp.watch(['src/index.js'], ['copy-index']);
 });
 
 
+gulp.task('browserifing', function () {
+    // set up the browserify instance on a task basis
+    var b = browserify({
+        entries: './build/js/all.js',
+        debug: true
+    });
+
+    return b.bundle()
+        .pipe(source('all.js'))
+        .pipe(buffer())
+        .pipe(sourceMaps.init({ loadMaps: true }))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .pipe(sourceMaps.write('./'))
+        .pipe(gulp.dest('./build/js/'));
+});
+
 // Concatenation of the build process to make it available into npm
 gulp.task('serve', function (callback) {
-    runSequence('clean', 'copy-index','copy-jslibs', 'copy-sw', 'copy-bootstrap', 'compile-sass', 'scripts-dist', 'templates','server','watch', callback);
+    runSequence('clean', 'copy-index', 'copy-jslibs', 'copy-sw', 'copy-bootstrap', 'compile-sass', 'scripts-dist','browserifing','templates','server','watch', callback);
 });
 
 
